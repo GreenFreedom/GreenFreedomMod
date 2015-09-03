389a021d24d3495542797492019fa30ad37b084f
OutOfYourMind <outofyourmindyt@gmail.com>
2015-09-03 13:50:54 -0400

    Update Command_trainingmode.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_trainingmode.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_trainingmode.java
@@ -7,7 +7,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-@CommandPermissions(level = AdminLevel.SENIOR, source = SourceType.ONLY_CONSOLE, blockHostConsole = true)
+@CommandPermissions(level = AdminLevel.SENIOR, source = SourceType.BOTH)
 @CommandParameters(description = "Start a training mode session.", usage = "/<command> [on | off]", aliases = "tm")
 public class Command_trainingmode extends TFM_Command
 {
