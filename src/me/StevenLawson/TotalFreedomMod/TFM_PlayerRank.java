c78c5aae0f82bf765bbec5e4e60c2ac34ce75e13
OutOfYourMind <outofyourmindyt@gmail.com>
2015-09-03 18:11:15 -0400

    Update TFM_PlayerRank.java

--- src/me/StevenLawson/TotalFreedomMod/TFM_PlayerRank.java
+++ src/me/StevenLawson/TotalFreedomMod/TFM_PlayerRank.java
@@ -32,6 +32,7 @@ public enum TFM_PlayerRank
     CO_OWNER("a " + ChatColor.BLUE + "Co-Owner" + ChatColor.AQUA + " of " + ChatColor.RED + "RubyFreedom", ChatColor.BLUE + "[Co-Owner]"),
     MYSTERI("a " + ChatColor.GOLD + "Potato Caek", ChatColor.RED + "[Mysteri]"),
     DARK("an " + ChatColor.BLUE + "Owner" + ChatColor.AQUA + ", " + ChatColor.BLUE + "Founder" + ChatColor.AQUA + ", and the " + ChatColor.DARK_PURPLE + "Co-Chief Developer", ChatColor.translateAlternateColorCodes('&', "&8[&4Owner &8+ &9Founder&8]&9")),
+    CHAR("the " + ChatColor.DARK_PURPLE+ "Co Chief Forum Dev" + ChatColor.DARK_PURPLE + ChatColor.AQUA + ", and a " + ChatColor.DARK_RED + "System-Admin", ChatColor.DARK_PURPLE + "§8[§5Co-Chief F-Dev§8]"),
     CONSOLE("the " + ChatColor.DARK_PURPLE + "Console", ChatColor.DARK_PURPLE + "[Console]");
     private final String loginMessage;
     private final String prefix;
@@ -109,6 +110,11 @@ public enum TFM_PlayerRank
         else if (sender.getName().equals("DarkGamingDronze"))
         {
             return DARK;
+        }
+        
+        else if (sender.getName().equals("Charlotte474747"))
+        {
+            return CHAR;
         }
 
         else if (FOP_DEVELOPERS.contains(sender.getName()))
