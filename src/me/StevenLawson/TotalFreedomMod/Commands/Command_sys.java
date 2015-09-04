e85668306d466fdaebf81411e725eb7c29329fbe
Tyler Wrenn <mcbeat257@gmail.com>
2015-09-04 17:55:38 -0400

    Revert "Update Command_sys.java"
    This reverts commit 679bf53a2fe30410d8e6632f19826f5bba6df29b.

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_sys.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_sys.java
@@ -103,7 +103,7 @@ public class Command_sys extends TFM_Command
 
         if (mode.equals("suspend"))
         {
-            if (!TFM_Util.RF_DEVELOPERS.contains(sender.getName()) && !TFM_Util.SYS.contains(sender.getName()) && !TFM_ConfigEntry.SERVER_OWNERS.getList().contains(sender.getName()))
+            if (!TFM_Util.RF_DEVELOPERS.contains(sender.getName()) && !TFM_ConfigEntry.SERVER_OWNERS.getList().contains(sender.getName()))
             {
                 sender.sendMessage(TFM_Command.MSG_NO_PERMS);
                 return true;
