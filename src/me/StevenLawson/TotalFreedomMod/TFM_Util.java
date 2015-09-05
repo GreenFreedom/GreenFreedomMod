739f66bb834c5c6bd48e7e06c49ebf1af88ed48e
Tyler Wrenn <mcbeat257@gmail.com>
2015-09-04 20:31:04 -0400

    Admin wipe, I think I forgot to commit crap

--- src/me/StevenLawson/TotalFreedomMod/TFM_Util.java
+++ src/me/StevenLawson/TotalFreedomMod/TFM_Util.java
@@ -72,8 +72,8 @@ public class TFM_Util
     public static final List<String> DEVELOPERS = Arrays.asList("Madgeek1450", "Prozza", "DarthSalmon", "AcidicCyanide", "Wild1145", "WickedGamingUK");
     public static final List<String> FOP_DEVELOPERS = Arrays.asList("Paldiu", "xDestroyer217", "Freelix2000", "Cyro1999"); // this isn't used, but we are keeping it for credit reasoning
     public static final List<String> RF_DEVELOPERS = Arrays.asList("Valencia_Orange", "AwesomePinch", "Alco_Rs11");
-    public static final List<String> EXECUTIVES = Arrays.asList("Alosion", "ItzTrae", "DecstarPlayzMC");
-    public static final List<String> SYS = Arrays.asList("cowgomooo12", "MysteriAce", "eddieusselman", "xYurippe", "Stampy100", "DarkHorse108", "Charlotte474747");
+    public static final List<String> EXECUTIVES = Arrays.asList("Alosion");
+    public static final List<String> SYS = Arrays.asList("DarkHorse108");
     public static final List<String> SPECIALISTS = Arrays.asList("reuben4545");
     public static final List<String> COOWNER = Arrays.asList("TaahThePenguin", "AndySixx");
     private static final Random RANDOM = new Random();
